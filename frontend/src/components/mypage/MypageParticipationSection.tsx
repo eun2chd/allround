baf95d1f8c1a24aa6e9b325d0ac2e0e0594c47c7
eunchong <tdc8165@naver.com>
2026-05-26 23:06:44 +0900

    빌드에러 수정

--- frontend/src/components/mypage/MypageParticipationSection.tsx
+++ frontend/src/components/mypage/MypageParticipationSection.tsx
@@ -1,5 +1,4 @@
 import { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
 import { useConfirm } from '../../context/ConfirmContext'
 import { contestFocusPath } from '../../features/contests/contestTypes'
 import { appToast } from '../../lib/appToast'
